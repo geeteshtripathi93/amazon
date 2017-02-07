c5c15d20087e2bd0b7ffe66437b31a0b3bdec98e
Kshitij Jain <kjain15@sapient.com>
2017-02-07 14:29:25 +0530

    Customer & Product
    Signed-off-by: Kshitij Jain <kjain15@sapient.com>

--- sapient amazon/src/com/project/bean/Customer.java
+++ sapient amazon/src/com/project/bean/Customer.java
@@ -1,5 +1,67 @@
 package com.project.bean;
 
 public class Customer {
+	private int cust_id;
+	private String cust_fname;
+	private String cust_lname;
+	private String cust_email;
+	private String cust_pass;
+	private String cust_phone;
+	
+	public Customer (int cust_id, String cust_fname, String cust_lname, String cust_email, String cust_pass, String cust_phone) {
+		this.cust_id = cust_id;
+		this.cust_fname = cust_fname;
+		this.cust_lname = cust_lname;
+		this.cust_email = cust_email;
+		this.cust_pass = cust_pass;
+		this.cust_phone = cust_phone;
+	}
 
+	public int getcust_id() {
+		return cust_id;
+	}
+
+	public void setcust_id(int cust_id) {
+		this.cust_id = cust_id;
+	}
+
+	public String getcust_fname() {
+		return cust_fname;
+	}
+
+	public void setcust_fname(String cust_fname) {
+		this.cust_fname = cust_fname;
+	}
+
+	public String getcust_lname() {
+		return cust_lname;
+	}
+
+	public void setcust_lname(String cust_lname) {
+		this.cust_lname = cust_lname;
+	}
+
+	public String getcust_email() {
+		return cust_email;
+	}
+
+	public void setcust_email(String cust_email) {
+		this.cust_email = cust_email;
+	}
+
+	public String getcust_pass() {
+		return cust_pass;
+	}
+
+	public void setcust_pass(String cust_pass) {
+		this.cust_pass = cust_pass;
+	}
+
+	public String getcust_phone() {
+		return cust_phone;
+	}
+
+	public void setcust_phone(String cust_phone) {
+		this.cust_phone = cust_phone;
+	}
 }
--- sapient amazon/src/com/project/bean/Product.java
+++ sapient amazon/src/com/project/bean/Product.java
@@ -1,5 +1,67 @@
 package com.project.bean;
 
 public class Product {
+	private int p_id;
+	private String p_name;
+	private String p_cat;
+	private String p_price;
+	private String p_qty;
+	private String p_disc;
+	
+	public Product(int p_id, String p_name, String p_cat, String p_price, String p_qty, String p_disc) {
+		this.p_id = p_id;
+		this.p_name = p_name;
+		this.p_cat = p_cat;
+		this.p_price = p_price;
+		this.p_qty = p_qty;
+		this.p_disc = p_disc;
+	}
 
+	public int getP_id() {
+		return p_id;
+	}
+
+	public void setP_id(int p_id) {
+		this.p_id = p_id;
+	}
+
+	public String getP_name() {
+		return p_name;
+	}
+
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+
+	public String getP_cat() {
+		return p_cat;
+	}
+
+	public void setP_cat(String p_cat) {
+		this.p_cat = p_cat;
+	}
+
+	public String getP_price() {
+		return p_price;
+	}
+
+	public void setP_price(String p_price) {
+		this.p_price = p_price;
+	}
+
+	public String getP_qty() {
+		return p_qty;
+	}
+
+	public void setP_qty(String p_qty) {
+		this.p_qty = p_qty;
+	}
+
+	public String getP_disc() {
+		return p_disc;
+	}
+
+	public void setP_disc(String p_disc) {
+		this.p_disc = p_disc;
+	}
 }
