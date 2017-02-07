13e9c0f40affc1eb19512dfcc617c370a756eab5
ssahu9 <sumit.ps94@gmail.com>
2017-02-07 14:07:23 +0530

    created connection helper

---
+++ sapient amazon/src/com/project/helper/CreateConnection.java
@@ -0,0 +1,24 @@
+package com.project.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class CreateConnection{
+	
+	private Connection con=null;
+ 
+	public	Connection getCon() throws ClassNotFoundException, SQLException
+	{
+		//Registering Driver - Connect
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		//Connect to database
+		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521",
+				"system", "sapient@123");
+	return con;
+	}
+	
+
+
+}
