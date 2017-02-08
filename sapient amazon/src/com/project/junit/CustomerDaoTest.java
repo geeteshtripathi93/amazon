9a919983b6ae2a7e15800d3e992aa6f0a30707dd
Kshitij Jain <kjain15@sapient.com>
2017-02-08 13:24:55 +0530

    CustomerDaoImpl JUnit

---
+++ sapient amazon/src/com/project/junit/CustomerDaoTest.java
@@ -0,0 +1,73 @@
+package com.project.junit;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.project.bean.Customer;
+import com.project.dao.CustomerDaoImpl;
+
+public class CustomerDaoTest {
+	CustomerDaoImpl customerDao;
+	Customer customer;
+
+	@Before
+	public void setUp() throws Exception {
+		customerDao = new CustomerDaoImpl();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		customerDao = null;
+	}
+
+	@Test(expected = SQLException.class)
+	public void negativeNumberTestInsert() throws ClassNotFoundException, SQLException {
+		customer = new Customer(-4, "aa", "ll", "hsh", "ghsg", "561420");
+		assertFalse(customerDao.insert(customer));
+		System.out.println("Customer id can't be negative");
+	}
+	
+	@Test(expected = SQLException.class)
+	public void positiveNumberTestInsert() throws ClassNotFoundException, SQLException {
+		customer = new Customer(4, "aa", "ll", "hsh", "ghsg", "561420");
+		assertTrue(customerDao.insert(customer));
+	}
+
+	@Test(expected = SQLException.class)
+	public void notNumberTestInsert() throws ClassNotFoundException, SQLException {
+		customer = new Customer('a', "aa", "ll", "hsh", "ghsg", "561420");
+		assertFalse(customerDao.insert(customer));
+		System.out.println("Customer id can't be a character");
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void positiveTestValidation() throws ClassNotFoundException, SQLException {
+		customer = new Customer(4, "aa", "ll", "hsh", "ghsg", "561420");
+		assertTrue(customerDao.validation("hsh", "ghsg"));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void negativeTestValidation() throws ClassNotFoundException, SQLException {
+		customer = new Customer(4, "aa", "ll", "hsh", "ghsg", "561420");
+		assertFalse(customerDao.validation("hsh", "ghs"));
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void negativeTestUpdate() throws ClassNotFoundException, SQLException {
+		customer = new Customer(-4, "aa", "ll", "hsh", "ghsg", "561420");
+		assertFalse(customerDao.update(customer));
+		System.out.println("Customer id can't be negative");
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void positiveTestUpdate() throws ClassNotFoundException, SQLException {
+		customer = new Customer(4, "aa", "ll", "hsh", "ghsg", "561420");
+		assertTrue(customerDao.update(customer));
+	}
+
+}
