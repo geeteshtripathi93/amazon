404e98e17926e7baba1163cb8ed7bb036feef466
ssahu9 <sumit.ps94@gmail.com>
2017-02-08 18:05:46 +0530

    BillDetail class and billDetails method implementation

--- sapient amazon/src/com/project/dao/BillDao.java
+++ sapient amazon/src/com/project/dao/BillDao.java
@@ -1,5 +1,6 @@
 package com.project.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.project.bean.Bill;
@@ -7,7 +8,7 @@ import com.project.bean.BillDetails;
 
 public interface BillDao {
 Bill generateBill(int customerId);
-List<BillDetails> getBillDetails(int customerId);
-List<BillDetails> getAllBillDetails();
+List<BillDetails> getBillDetails(int customerId)  throws ClassNotFoundException, SQLException;
+List<BillDetails> getAllBillDetails()throws  ClassNotFoundException,SQLException;
 
 }
