4990550a2c25fa2a218d26152019a6b10ad40ec8
Ishita Mahindroo <imahin@sapient.com>
2017-02-08 12:54:02 +0530

    skeleton

--- sapient amazon/src/com/project/junit/AdminUnit.java
+++ sapient amazon/src/com/project/junit/AdminUnit.java
@@ -2,28 +2,34 @@ package com.project.junit;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class AdminUnit {
+import com.project.dao.AdminDaoImpl;
 
+public class AdminUnit {
+AdminDaoImpl admin;
 	@Before
 	public void setUp() throws Exception {
+		admin=new AdminDaoImpl();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		admin=null;
 	}
 
 	@Test
-	public void testValidation() {
-		fail("Not yet implemented");
+	public void testValidation() throws ClassNotFoundException, SQLException {
+		assertTrue(admin.validation("ishita", "ishita"));
 	}
 
 	@Test
 	public void testUpdate() {
-		fail("Not yet implemented");
+		
 	}
 
 }
---
+++ sapient amazon/src/com/project/junit/CartUnit.java
@@ -0,0 +1,43 @@
+package com.project.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.project.dao.CartDaoImpl;
+
+public class CartUnit {
+CartDaoImpl cart;
+	@Before
+	public void setUp() throws Exception {
+		cart= new CartDaoImpl();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		cart=null;
+	}
+
+	@Test
+	public void testAddToCart() {
+		
+	}
+
+	@Test
+	public void testRemoveFromCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testViewCart() {
+		fail("Not yet implemented");
+	}
+
+}
