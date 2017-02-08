ab2531ea6ba4280c0535d7d23a3eaa9a734e15f4
Ishita Mahindroo <imahin@sapient.com>
2017-02-08 17:40:35 +0530

    noteleks

---
+++ sapient amazon/src/com/project/bl/CustomerBl.java
@@ -0,0 +1,29 @@
+package com.project.bl;
+
+public class CustomerBl {
+
+	
+	public void signIn(){
+		
+	}	
+	
+	public void signUp(){
+		
+	}
+	public void viewCart(){
+		
+	}
+	
+	public void viewCategory(){
+		
+	}
+	
+	public void history(){
+		
+	}
+	
+	public void search(){
+		
+	}
+	
+}
