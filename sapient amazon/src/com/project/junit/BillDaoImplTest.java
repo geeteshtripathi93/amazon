c7c0feb84512d80490178be54f4d0f8bbc33827d
Kshitij Jain <kjain15@sapient.com>
2017-02-09 11:09:00 +0530

    BillJUnit

---
+++ sapient amazon/src/com/project/junit/BillDaoImplTest.java
@@ -0,0 +1,72 @@
+package com.project.junit;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.project.bean.Bill;
+import com.project.bean.BillDetails;
+import com.project.bean.Customer;
+import com.project.dao.BillDaoImpl;
+
+public class BillDaoImplTest {
+
+	BillDaoImpl billDao;
+	Customer customer;
+	Bill bill;
+	String str = "2017-02-08";
+	Date date = Date.valueOf(str);
+	
+	@Before
+	public void setUp() throws Exception {
+		billDao = new BillDaoImpl();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		billDao = null;
+	}
+
+	@Test
+	public void positiveGenerateBill() {
+		customer = new Customer(1, "Sherlock", "Holmes", "221B", "SH", "221221");
+		assertEquals(1, customer.getCustomerId());
+	}
+
+	@Test
+	public void negativeGenerateBill() {
+		customer = new Customer(1, "Sherlock", "Holmes", "221B", "SH", "221221");
+		assertNotEquals(2, customer.getCustomerId());
+	}
+	
+	@Test
+	public void positiveGetBillDetails() {
+		customer = new Customer(1, "Sherlock", "Holmes", "221B", "SH", "221221");
+		assertEquals(1, customer.getCustomerId());
+	}
+	
+	@Test
+	public void negativeGetBillDetails() {
+		customer = new Customer(1, "Sherlock", "Holmes", "221B", "SH", "221221");
+		assertNotEquals(2, customer.getCustomerId());
+	}
+
+	@Test
+	public void positiveGetCurrentBill() {
+		bill = new Bill(1, 1, date);
+		assertEquals(1, bill.getBillId());
+	}
+
+	@Test
+	public void negativeGetCurrentBill() {
+		bill = new Bill(1, 1, date);
+		assertNotEquals(2, bill.getBillId());
+	}
+
+}
